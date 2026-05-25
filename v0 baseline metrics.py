4d24c9feaf8122e38c5c3deceb0dbb8b9812fbc5
Saoud Al-Emadi <saoude@andrew.cmu.edu>
2026-05-25 23:18:49 +0300

    Add files via upload

---
+++ v0 baseline metrics.py
@@ -0,0 +1,83 @@
+#Imported a class (textatistic) to calculate text complexity and readability scores
+from textatistic import Textatistic
+
+#METRIC 1: READABILITY (Flesch-Kincaid)
+""" Resource used for implementation
+https://youtu.be/tw7sEhfRZxM?si=znk3iABTSrdmKPXk """
+def readability(course_quality):
+    #Score based on Flesch-Kincaid formula
+    score = Textatistic(course_quality).scores['flesch_score']
+    
+    #Scaled the score to give a readability score from 1 (difficult) to 5 (easy)
+    if score >= 90.0:
+        return 5 
+    elif score >= 70.0:
+        return 4  
+    elif score >= 50.0:
+        return 3  
+    elif score >= 30.0:
+        return 2  
+    else:
+        return 1
+
+
+
+#METRIC 2: STRUCTURE HEURISTICS
+''' Basic (not complete) implementaion of counting the number of appearnces
+    of the title, heading, sub-heading and returning a score based on it '''
+def structure(course_quality):
+    levels_found = set()
+    
+    ''' 1 for #, 2 for ##, 3 for ### '''
+    prev_level = 0 
+    
+    lines = course_quality.splitlines()
+    
+    for l in lines:
+        line = l.strip()
+        currentl_level = 0
+        
+        if (line.startswith("### ")):
+            currentl_level = 3
+            
+        elif (line.startswith("## ")):
+            currentl_level = 2
+            
+        elif (line.startswith("# ")):
+            currentl_level = 1
+        
+        # Check the order immediately
+        if (current_level > 0):
+            ''' CRITICAL CHECK: You can never skip a level going down (e.g.,
+                going from the tile straight to sub-heading is not allowed) '''
+            if prev_level == 1 and current_level == 3:
+                return 1
+            
+            levels_found.add(current_level)
+            prev_level = current_level
+            
+    # Returning a score from 1 to 5 based on logic based on our counts
+    calculated_score = 1 + len(levels_found)
+    if (calculated_score == 4):
+        return 5
+    return calculated_score
+
+
+
+
+
+
+#TESTING
+"""
+INSTRUCTIONS: to test with files, you must convert them manually to a
+block of strings
+"""
+    
+#METRIC 1
+easy = "this is for testing."
+hard = "pneumonoultramicroscopicsilicovolanoconiosis."
+print("Score 1: ", readability(easy))
+print("Score 2: ", readability(hard))    
+    
+
+    
